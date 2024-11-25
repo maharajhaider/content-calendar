e375cedc5eb62a755ed94b44e2fe0c8431d88448
Maharaj Haider <maharajhaider@hotmail.com>
2024-11-24 21:37:11 -0800

    added config

---
+++ src/main/java/com/maharaj/content_calendar/Message.java
@@ -0,0 +1,11 @@
+package com.maharaj.content_calendar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+    public String getMessage() {
+        return "Hello World";
+    }
+
+}
